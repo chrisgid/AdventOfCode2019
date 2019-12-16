d1da3ca513544736bceeb10407517b04967909d6
Christopher Gidney <chris.gidney@live.co.uk>
2019-12-16 22:12:22 +0000

    Started day 5

---
+++ src/AdventOfCode2019/Day5/Computer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019.Day5
+{
+    public class Computer
+    {
+        private int[] _memory;
+        private int _instructionPointer;
+
+        public Computer(int[] program)
+        {
+            _memory = (int[])program.Clone();
+            _instructionPointer = 0;
+        }
+    }
+
+    public enum Opcode
+    {
+        Add = 1,
+        Multiply = 2,
+        Input = 3,
+        Output = 4,
+        Halt = 99
+    }
+}
---
+++ src/AdventOfCode2019/Day5/Day5.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day5
+{
+    public class Day5 : BaseDay
+    {
+        public override string GetPartOneAnswer()
+        {
+            var input = ReadInput();
+            return base.GetPartOneAnswer();
+        }
+
+        public override string GetPartTwoAnswer()
+        {
+            return base.GetPartTwoAnswer();
+        }
+
+        private static int[] ReadInput()
+        {
+            string input;
+
+            using (var stream = InputHelper.GetInputTxtStream())
+            using (var reader = new StreamReader(stream))
+            {
+                input = reader.ReadToEnd();
+            }
+
+            var strings = input.Split(',');
+
+            return strings.Select(int.Parse).ToArray();
+        }
+    }
+}
